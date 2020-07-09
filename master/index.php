bed119401243235b46ef8783ca29f197d778956c
Manideepto <dasmanideepto@gmail.com>
2020-07-10 00:55:09 +0530

    New UI for recent events and Blogs

--- master/index.php
+++ master/index.php
@@ -6,75 +6,7 @@
 		header('location:'.BASE_URL.'');
 	}
 	
-	//Query for Events
-	/*$sql = "SELECT event_id,event_title,event_thumbnail FROM mp_events WHERE org_id = '" .$_SESSION['org_id']. "'AND status='A' ORDER BY event_date,count_interested ASC";
-	$stmt = $DB->prepare($sql);
-	$stmt->execute();
-	$results = $stmt->fetchAll();
-	$i=0;
-	echo "<div class='navbar'>";
-	foreach($results as $row) {
-		if($i==2)
-			break;
-		echo "<a target='_blank' href='event_details.php?event_id=".$row['event_id']."'><img src='".$row['event_thumbnail']."' style='width:100px'><br>".$row['event_title']."</a>";
-		$i++;
-	}
-	
-	//Query for Blogs
-	$sql = "SELECT blog_id,blog_title,photo FROM mp_blogs WHERE org_id = '" .$_SESSION['org_id']. "'AND status='A' ORDER BY date ASC";
-	$stmt = $DB->prepare($sql);
-	$stmt->execute();
-	$results = $stmt->fetchAll();
-	$i=0;
-	foreach($results as $row) {
-		if($i==2)
-			break;
-		echo "<a target='_blank' href='blog_details.php?blog_id=".$row['blog_id']."'><img src='".$row['photo']."' style='width:100px'><br>".$row['blog_title']."</a>";
-		$i++;
-	}
-	echo "</div>";*/
-	
 ?>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<style>
-	* {
-	  box-sizing: border-box;
-	}
-
-	/* Create three equal columns that floats next to each other */
-	.column {
-	  float: left;
-	  width: 25%;
-	  padding: 10px;
-	  height: 200px; /* Should be removed. Only for demonstration */
-	}
-
-	/* Clear floats after the columns */
-	.row:after {
-	  content: "";
-	  display: table;
-	  clear: both;
-	}
-	</style>
-	
-	<div class="row">
-	<div class="column" style="background-color:#dddd;">
-		<h2 style="text-align:right"><u>Upcoming</u></h2>
-		<center><p id="upcoming_event1"></p></center>
-	 </div>
-	 <div class="column" style="background-color:#cccc;" >
-		<h2><u>Events</u></h2>
-		<center><p id="upcoming_event2"></p></center>
-	 </div>
-	 <div class="column" style="background-color:#eedf;">
-		<h2 style="text-align:right"><u>Recent</u></h2>
-		<center><p id="recent_blog1"></p></center>
-	 </div>
-	 <div class="column" style="background-color:#ddcf;" >
-		<h2><u>Blogs</u></h2>
-		<center><p id="recent_blog2"></p></center>
-	 </div>
-	 </div>
 	
 	<div>
 	<aside id="fh5co-hero">
@@ -134,6 +66,63 @@
 
 	</div>
 	
+	 
+	<div id="fh5co-course">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+					<h2><b>Upcoming Events & Recent Blogs<b></h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-6 animate-box">
+					<div class="course">
+						<a href="#" class="course-img" id="recent_event_img1" style="background-image: url(images/project-1.jpg);">
+						</a>
+						<div class="desc">
+							<h3><a href="#" id="recent_event_title1"></a></h3>
+							<a id="recent_event_desc1"></a><br><br>
+							<span><a class="btn btn-primary btn-sm btn-course" id="recent_event_details1">Event Details</a></span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 animate-box">
+					<div class="course">
+						<a href="#" class="course-img" id="recent_event_img2" style="background-image: url(images/project-2.jpg);">
+						</a>
+						<div class="desc">
+							<h3><a href="#" id="recent_event_title2"></a></h3>
+							<a id="recent_event_desc2"></a><br><br>
+							<span><a class="btn btn-primary btn-sm btn-course" id="recent_event_details2">Event Details</a></span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 animate-box">
+					<div class="course">
+						<a href="#" class="course-img" id="recent_blog_img1" style="background-image: url(images/project-1.jpg);">
+						</a>
+						<div class="desc">
+							<h3><a href="#" id="recent_blog_title1"></a></h3>
+							<a id="recent_blog_desc1"></a><br><br>
+							<span><a class="btn btn-primary btn-sm btn-course" id="recent_blog_details1">Blog Details</a></span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6 animate-box">
+					<div class="course">
+						<a href="#" class="course-img" id="recent_blog_img2" style="background-image: url(images/project-2.jpg);">
+						</a>
+						<div class="desc">
+							<h3><a href="#" id="recent_blog_title2"></a></h3>
+							<a id="recent_blog_desc2"></a><br><br>
+							<span><a class="btn btn-primary btn-sm btn-course" id="recent_blog_details2">Blog Details</a></span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
 	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 	
 	<script type="text/javascript"> 
@@ -149,15 +138,38 @@
 			}
 			}).done(function( data ) { 
 			var result= $.parseJSON(data); 
-			var string = '';
+			var recent_event_title1 = document.getElementById('recent_event_title1');
+			var recent_event_title2 = document.getElementById('recent_event_title2');
+			var recent_event_desc1 = document.getElementById('recent_event_desc1');
+			var recent_event_desc2 = document.getElementById('recent_event_desc2');
+			var recent_event_img1 = document.getElementById('recent_event_img1');
+			var recent_event_img2 = document.getElementById('recent_event_img2');
+			var recent_event_details1 = document.getElementById('recent_event_details1');
+			var recent_event_details2 = document.getElementById('recent_event_details2');
+			
 			var i=0;
 			$.each( result, function( key, value ) {
 				i++;
-				string = "<a target='_blank' href='event_details.php?event_id="+value['event_id']+"'><img src='"+value['event_thumbnail']+"' class='img-responsive img-circle img-thumbnail' style='width: 100px; height: 100px;' align='middle'><h4>"+value['event_title']+"</h4></a>";
+				//string = "<a target='_blank' href='event_details.php?event_id="+value['event_id']+"'><img src='"+value['event_thumbnail']+"' class='img-responsive img-circle img-thumbnail' style='width: 100px; height: 100px;' align='middle'><h4>"+value['event_title']+"</h4></a>";
 				if(i==1)
-					$("#upcoming_event1").html(string);
+				{
+					recent_event_title1.text = value['event_title'];
+					recent_event_title1.href="event_details.php?event_id="+value['event_id'];
+					recent_event_desc1.text = value['event_desc'];
+					recent_event_img1.style.backgroundImage =  "url(" + value['event_thumbnail'] + ")";
+					recent_event_img1.href="event_details.php?event_id="+value['event_id'];
+					recent_event_details1.href="event_details.php?event_id="+value['event_id'];
+				}
 				else if(i==2)
-					$("#upcoming_event2").html(string);
+				{
+					recent_event_title2.text = value['event_title'];
+					recent_event_title2.href="event_details.php?event_id="+value['event_id'];
+					recent_event_desc2.text = value['event_desc'];
+					recent_event_img2.style.backgroundImage =  "url(" + value['event_thumbnail'] + ")";
+					recent_event_img2.href="event_details.php?event_id="+value['event_id'];
+					recent_event_details2.href="event_details.php?event_id="+value['event_id'];
+					
+				}
 			});
 			});
 			var page = 'recent_blogs';
@@ -169,15 +181,37 @@
 			}
 			}).done(function( data ) { 
 			var result= $.parseJSON(data);
-			var string = '';
+			var recent_blog_title1 = document.getElementById('recent_blog_title1');
+			var recent_blog_title2 = document.getElementById('recent_blog_title2');
+			var recent_blog_desc1 = document.getElementById('recent_blog_desc1');
+			var recent_blog_desc2 = document.getElementById('recent_blog_desc2');
+			var recent_blog_img1 = document.getElementById('recent_blog_img1');
+			var recent_blog_img2 = document.getElementById('recent_blog_img2');
+			var recent_blog_details1 = document.getElementById('recent_blog_details1');
+			var recent_blog_details2 = document.getElementById('recent_blog_details2');
+			
 			var i=0;
 			$.each( result, function( key, value ) {
 				i++;
 				string = "<a target='_blank' href='blog_details.php?blog_id="+value['blog_id']+"'><img src='"+value['photo']+"' class='img-responsive img-circle img-thumbnail' style='width: 100px; height: 100px;' align='middle'><h4>"+value['blog_title']+"</h4></a>";
 				if(i==1)
-					$("#recent_blog1").html(string);
+				{
+					recent_blog_title1.text = value['blog_title'];
+					recent_blog_title1.href="blog_details.php?blog_id="+value['blog_id'];
+					recent_blog_desc1.text = value['blog_data'];
+					recent_blog_img1.style.backgroundImage =  "url(" + value['photo'] + ")";
+					recent_blog_img1.href="blog_details.php?blog_id="+value['blog_id'];
+					recent_blog_details1.href="blog_details.php?blog_id="+value['blog_id'];
+				}
 				else if(i==2)
-					$("#recent_blog2").html(string);
+				{
+					recent_blog_title2.text = value['blog_title'];
+					recent_blog_title2.href="blog_details.php?blog_id="+value['blog_id'];
+					recent_blog_desc2.text = value['blog_data'];
+					recent_blog_img2.style.backgroundImage =  "url(" + value['photo'] + ")";
+					recent_blog_img2.href="blog_details.php?blog_id="+value['blog_id']
+					recent_blog_details2.href="blog_details.php?blog_id="+value['blog_id']
+				}
 			});
 			});
 			
