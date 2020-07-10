5568ae048b1417d5b114253e66fb63fac63eb2ec
roshanm0903 <roshanm0903@gmail.com>
2020-07-11 00:06:54 +0530

    ui corrections

--- master/blog.php
+++ master/blog.php
@@ -6,10 +6,9 @@
 	}
 ?>
 
-<div class="container">
-<div id="fh5co-course">
- 
-    <div class="row">
+<div id="fh5co-blog">
+
+</div>
 
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
    
@@ -27,26 +26,45 @@
            }
          }).done(function( data ) { 
            var result= $.parseJSON(data); 
-           var string = '';
+           var string = '<div class="container">\
+           <div class="row">';
    
           /* from result create a string of data and append to the div */
  
-		  
+          
 	       $.each( result, function( key, value ) { 
-			string += "<div class='col-md-6 animate-box'>\
-            <div class='course'>\
-            <br>\
-            <div class='desc'>\
-              <h3><a href='blog_details.php?blog_id="+value['blog_id']+"'>"+ value['blog_title'] + "</a></h3>\
-              <br>\
-              <p>"+ value['blog_data'].substring(0,80) + "</p>";
-      string +=   "</div>\
-                </div>\
-                </div>";
+			string += '<div class="col-lg-4 col-md-4">\
+					<div class="fh5co-blog animate-box">';
+						string += "<a href='blog_details.php?blog_id="+value['blog_id']+ " ' class='blog-img-holder' style='background-image: url("+value['photo'] + ");'>"+
+					    "</a>";
+            string += '<div class="blog-text">\
+							<h3><a href="blog_details.php?blog_id='+value['blog_id'] + ' ">' + value['blog_title']  + '</a></h3>\
+							<span class="posted_on">' +value["date"] + '</span>\
+							<p>' + value["blog_data"].substring(0,255).replace( /(<([^>]+)>)/ig, '') + '</p>\
+              <span><a class="btn btn-primary btn-sm btn-course" href="blog_details.php?blog_id= '+ value['blog_id'] + '"> Read</a></span>\
+            </div> \
+					</div>\
+				</div>';
+          
+
+
+      //       <div class='course'>\
+      //       <br>\
+      //       <div class='desc'>\
+      //         <h3><a href='blog_details.php?blog_id="+value['blog_id']+"'>"+ value['blog_title'] + "</a></h3>\
+      //         <br>\
+      //         <p>"+ value['blog_data'].substring(0,80).replace( /(<([^>]+)>)/ig, '') + "</p>";
+      // string +=   "</div>\
+      //           </div>\
+      //           </div>";
             
 				}); 
 			 
-       $("#fh5co-course").html(string);
+       string += '</div></div>';
+
+      //  string = string.replace(/upload/g,'upload/');
+
+       $("#fh5co-blog").html(string);
               // document.getElementById('tagline1').innerHTML = string; 
 			//   document.getElementById('backgroundimage1').style= "background-image: url(images/img_bg_1.jpg)";
    
